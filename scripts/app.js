f3f0390b405b2b793cf1270e4ae189d86e6c765d
Ignasi <ignblanch@gmail.com>
2017-07-25 14:03:30 +0200

    routes created

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-angular.module('infoWeatherApp', []);
+angular.module('infoWeatherApp', ['ngRoute'])
+	
+	.config( function( $routeProvider ) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: '../Views/search.html',
+        controller: 'SearchController'
+     })
+      .when('/results/:query', {
+        templateUrl: '../Views/results.html',
+        controller: 'ResultsController'
+     })
+
+  });
