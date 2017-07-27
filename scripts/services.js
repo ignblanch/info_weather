ee744eafdbd5b19222356215aea1f87cae194d9f
Ignasi <ignblanch@gmail.com>
2017-07-27 15:23:26 +0200

    passed js standard linter

--- scripts/services.js
+++ scripts/services.js
@@ -1,51 +1,44 @@
+/* global angular */
 angular.module('infoWeatherApp')
 
-	.service('WeatherService', function($http) {
-		var self = this;
+    .service('WeatherService', function ($http) {
+      var self = this
 
-		self.search = function(query, callback){
+      self.search = function (query, callback) {
+        self.query = query
 
-			self.query = query;
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.query + '&appid=68d52a9f0d93266ba323fbb2e254f29d&units=metric'
 
-			var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.query + '&appid=68d52a9f0d93266ba323fbb2e254f29d&units=metric';
+        $http.get(url)
+                .then(function (response) {
+                  console.log('succes')
+                  console.log(response)
 
-			$http.get(url)
-				.then(function(response){
+                  callback(response)
+                }, function error (response) {
+                  console.error('ko' + response)
+                })
+      }
 
-					console.log("succes");
-					console.log(response);
+      self.listCities = function (callback) {
+        $http.get('../assets/spain_city_list.json')
+        .then(function (response) {
+          callback(response)
+        })
+      }
 
-					callback(response);
+      self.fiveDays = function (query, callback) {
+        self.query = query
 
-				}, function error(response){
-					console.error("ko" + response);
-				})
+        var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + query + '&appid=68d52a9f0d93266ba323fbb2e254f29d&units=metric'
 
-			}
-
-			self.listCities = function (callback) {
-            	$http.get('../assets/spain_city_list.json')
-                	.then(function (response){
-                    	callback(response);
-            	})
-        	}
-
-			self.fiveDays = function(query, callback){
-
-				self.query = query;
-
-				var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + query + '&appid=68d52a9f0d93266ba323fbb2e254f29d&units=metric';
-
-				$http.get(url)
-				.then(function(response){
-
-					console.log("succes");
-					console.log(response);
-					callback(response);
-
-					}, function error(response){
-					console.error("ko" + response);
-				})
-			}
-	});
-
+        $http.get(url)
+                .then(function (response) {
+                  console.log('succes')
+                  console.log(response)
+                  callback(response)
+                }, function error (response) {
+                  console.error('ko' + response)
+                })
+      }
+    })
